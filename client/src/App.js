302396efaca9ba741aa404f1aa475c48e8fe7565
Jfelipemendez21 <jfelipemendez@ucundinamarca.edu.co>
2023-05-18 17:42:26 -0500

    Version 1.3 chat

--- client/src/App.js
+++ client/src/App.js
@@ -121,7 +121,7 @@ function App() {
            <small className='text-center text-muted'>...Mensajes guardados...</small>
                 {storeMessages.map((message, index) => (
                   <div key={index} className={`d-flex p-3 ${message.from===nickname ? "justify-content-end" : "justify-content-start"}`}>
-                    <div className={`card mb-3 border-1 ${message.from===nickname ? "bg-success color-light bg-opacity-25" : "bg-light"}`}>
+                    <div className={`card mb-3 border-1 ${message.from===nickname ? "bg-success text-light bg-opacity-25" : "bg-light"}`}>
                       <div className='card-body'>
                         <small className='text-muted'>{message.from}: {message.message}</small>
                       </div>
